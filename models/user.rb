49a7ee57af0a9a2a20a6853afeecc0ae0b4921bc
Matt DuBois <mattdubois2@gmail.com>
2015-03-23 15:37:39 -0500

    user model added

---
+++ models/user.rb
@@ -0,0 +1,19 @@
+class User
+
+  include ApplicationHelper
+
+  attr_reader :first_name, :last_name, :gender, :favorite_color, :date_of_birth
+
+  def initialize(user_params)
+    @first_name = user_params.fetch(:first_name,nil)
+    @last_name = user_params.fetch(:last_name,nil)
+    @gender = user_params.fetch(:gender,nil)
+    @favorite_color = user_params.fetch(:favorite_color,nil)
+    @date_of_birth = user_params.fetch(:date_of_birth,nil)
+  end
+
+  def render
+    "#{@last_name} #{@first_name} #{@gender} #{@favorite_color} #{formatted_date(@date_of_birth)}"
+  end
+
+end
