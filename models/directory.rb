c623d95ee0d48213783b6a43715b02f8ace26708
Matt DuBois <mattdubois2@gmail.com>
2015-03-23 14:56:58 -0500

    directory model and sorting users array

--- models/directory.rb
+++ models/directory.rb
@@ -1,2 +1,24 @@
 class Directory
+
+  attr_reader :users
+
+  def initialize(users = [])
+    @users = users
+  end
+
+  def add_user(user)
+    @users << user
+  end
+
+  def sort_by_gender
+    @users.sort_by{ |u| [u.gender, u.last_name] }
+  end
+
+  def sort_by_date_of_birth
+    @users.sort_by{ |u| u.date_of_birth}
+  end
+
+  def sort_by_last_name
+    @users.sort_by{|u| u.last_name}.reverse
+  end
 end
