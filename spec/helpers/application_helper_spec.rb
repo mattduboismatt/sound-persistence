781768dc34a8fff1dd747faf6a9b21f3158d6e4b
Matt DuBois <mattdubois2@gmail.com>
2015-03-23 15:23:31 -0500

    #formatted_date helper method and spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper.rb'
 
 describe ApplicationHelper do
   include ApplicationHelper
+
+  describe '#formatted_date' do
+    it 'returns date in M/D/YYYY format' do
+      expect(formatted_date(Date.parse('1998-01-07'))).to eq "1/7/1998"
+    end
+    it 'returns date in M/D/YYYY format' do
+      expect(formatted_date(Date.parse('2005-06-27'))).to eq "6/27/2005"
+    end
+  end
 end
