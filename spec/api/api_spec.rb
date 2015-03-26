7d9f555fd0416c0899fcad7cc662620af65f5489
Matt DuBois <mattdubois2@gmail.com>
2015-03-26 15:11:38 -0500

    specs for API get actions

--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -9,17 +9,64 @@ describe SoundPersistence::API do
     SoundPersistence::API
   end
 
-  let(:directory) { $controller.directory }
-  let(:first_record) { 'Zzz Mary Female Red 1900-01-01' }
-  let(:last_record) { 'Aaa Mike Male Red 2015-01-01' }
+  let(:space_record) { 'Aaa Mike Male Red 2015-01-01' }
+  let(:comma_record) { 'Aaa, Mike, Male, Red, 2015-01-01' }
+  let(:pipe_record) { 'Aaa | Mike | Male | Red | 2015-01-01' }
+
+  before :all do
+    first_user = User.new({
+      first_name: 'Mary',
+      last_name: 'Aaa',
+      gender: 'Female',
+      favorite_color: 'Red',
+      date_of_birth: Date.parse('1900-01-01')
+    })
+    directory = $controller.directory
+    directory.add_user(first_user)
+  end
 
   describe SoundPersistence::API do
-    describe "GET /records/gender" do
-      it "returns directory records sorted by gender, as json" do
-        get "/records/gender"
-        expect(last_response.status).to eq(200)
-        expect(JSON.parse(last_response.body)).to eq "test"
+    describe 'GET' do
+
+      context '/records/gender' do
+        it "returns directory records as json" do
+          get "/records/gender"
+          expect(last_response.status).to eq(200)
+          expect(last_response.header['Content-Type']).to include 'application/json'
+        end
+        it 'correctly sorts records by gender, then last name descending' do
+          get "/records/gender"
+          expect(JSON.parse(last_response.body)['users'].count).to eq 7
+          expect(JSON.parse(last_response.body)['users'].first['last_name']).to eq 'Aaa'
+        end
       end
+
+      context '/records/birthdate' do
+        it "returns directory records as json" do
+          get "/records/birthdate"
+          expect(last_response.status).to eq(200)
+          expect(last_response.header['Content-Type']).to include 'application/json'
+        end
+        it 'correctly sorts records by date of birth, ascending' do
+          get "/records/birthdate"
+          expect(JSON.parse(last_response.body)['users'].count).to eq 7
+          expect(JSON.parse(last_response.body)['users'].first['date_of_birth']).to eq '1/1/1900'
+        end
+      end
+
+      context '/records/name' do
+        it "returns directory records as json" do
+          get "/records/name"
+          expect(last_response.status).to eq(200)
+          expect(last_response.header['Content-Type']).to include 'application/json'
+        end
+        it 'correctly sorts records by last name, descending' do
+          get "/records/name"
+          expect(JSON.parse(last_response.body)['users'].count).to eq 7
+          expect(JSON.parse(last_response.body)['users'].last['last_name']).to eq 'Aaa'
+        end
+      end
+
     end
   end
 end
